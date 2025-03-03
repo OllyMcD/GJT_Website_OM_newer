e9d817f921ac9102d3c1a98a2e348f4a1760e2cf
S2303831 <S2303831@NDC03196>
2025-03-03 11:22:58 +0000

    - Minor Changes

---
+++ GJT_Website_OM/wwwroot/js/sound.js
@@ -0,0 +1,15 @@
+﻿window.playAudio = function () {
+    let audio = document.getElementById("myAudio");
+
+    if (audio) {
+        let playPromise = audio.play();
+
+        if (playPromise !== undefined) {
+            playPromise
+                .then(() => console.log("Audio playing"))
+                .catch(error => console.error("Autoplay failed:", error));
+        }
+    } else {
+        console.error("Audio element not found");
+    }
+};
